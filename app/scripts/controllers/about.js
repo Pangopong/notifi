2389966aba8d7cb34116bc8b7d322d0238da3b7e
ITS-NET\petroschi.matei <email@email.com>
2017-05-05 09:52:01 +0300

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name notifiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the notifiApp
+ */
+angular.module('notifiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name notifiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the notifiApp
+ */
+angular.module('notifiApp')
+  .controller('MainCtrl', function ($scope, $rootScope, $http) {
+  	
+  });
